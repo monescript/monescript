6e6d24d23153b6fdd6083bf4199d948947bba395
abtechbit <house_track@yahoo.ca>
2017-12-10 20:48:05 -0500

    journal: add tag extractor utility

---
+++ spec/parser/tag-extractor.spec.js
@@ -0,0 +1,71 @@
+fdescribe("Tag Extractor", function() {
+  var tagExtractor = require("../../src/parser/tag-extractor.js");
+
+/*
+https://www.ledger-cli.org/3.0/doc/ledger3.html#Metadata-tags
+
+To tag an item, put any word not containing whitespace between two colons inside a comment:
+  :TAG:
+
+You can gang up multiple tags by sharing colons:
+   :TAG1:TAG2:TAG3:
+
+https://www.ledger-cli.org/3.0/doc/ledger3.html#Metadata-values
+
+To associate a value with a tag, use the syntax “Key: Value”, where the value can be any string of characters. Whitespace is needed after the colon, and cannot appear in the Key:
+*/
+
+  it("returns nothing for empty", function() {
+    let tags = tagExtractor.extractTags(
+      " "
+    );
+    expect(tags).toEqual([]);
+  });
+
+  it("returns nothing if there are no tags", function() {
+    let tags = tagExtractor.extractTags(
+      " abcd "
+    );
+    expect(tags).toEqual([]);
+  });
+
+  it("returns no value tag with single colon", function() {
+    let tags = tagExtractor.extractTags(
+      "abcd:"
+    );
+    expect(tags).toEqual( [{ 'abcd': ''} ]);
+  });
+
+  it("returns no value tag with colons around it", function() {
+    let tags = tagExtractor.extractTags(
+      ":abcd:"
+    );
+    expect(tags).toEqual( [{ 'abcd': ''} ]);
+  });
+
+  it("returns multiple tags with colons around them", function() {
+    let tags = tagExtractor.extractTags(
+      ":abcd:efg:tag1:TAG2"
+    );
+    expect(tags).toEqual( [
+      { 'abcd': ''},
+      { 'efg': ''},
+      { 'tag1': ''},
+      { 'TAG2': ''},
+    ]);
+  });
+
+  it("returns tag with a value", function() {
+    let tags = tagExtractor.extractTags(
+      "abcd:efgh ijk lmn op"
+    );
+    expect(tags).toEqual([{'abcd':'efgh ijk lmn op'}]);
+  });
+
+  it("trims tag key", function() {
+    let tags = tagExtractor.extractTags(
+      "  abcd:efgh"
+    );
+    expect(tags).toEqual([{'abcd':'efgh'}]);
+  });
+})
---
+++ src/parser/tag-extractor.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  extractTags: function(value){
+    let tags = [];
+    let parts = value.split(':');
+
+    if(parts.length > 1){
+      if(parts.length == 2){
+        tags.push(this.createTag(parts[0], parts[1]));
+      } else {
+        let validTagNames = parts.filter(p => p != '' && p.indexOf(' ') == -1  && p.indexOf('\t') == -1);
+        validTagNames.forEach(t => {
+          tags.push(this.createTag(t, ''));
+        });
+      }
+    }
+    return tags;
+  },
+
+  createTag: function(key, value){
+    let tag = {};
+    tag[key.trim()] = value;
+    return tag;
+  }
+}
